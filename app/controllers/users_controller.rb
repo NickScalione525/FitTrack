b28ef84cb44c7e40f8d2167a0d048c89b8900453
nick <nicholas_scalione@yahoo.com>
2021-04-01 11:16:27 -0400

    added code to my Users#create method to allow a user to signup and be directed to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,24 @@ class UsersController < ApplicationController
 
 
     def create
-
+        @user = User.new(user_params)
+        if user.save
+            session[:user_id] = @user_id
+            redirect_to @user
+        else
+            render :new
+        end
     end
 
     def show
 
     end
+
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :email, :password)
+    end
+
 end
