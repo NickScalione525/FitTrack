4873666c64c9ad9049b778fccaa5e9f0857086e4
nick <nicholas_scalione@yahoo.com>
2021-04-02 13:38:06 -0400

    Added funcionality to update and destroy methods and implemented a confirmation prompt upon deletion of workout

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -28,13 +28,19 @@ class WorkoutsController < ApplicationController
         end
     end
     def update
-
+        if @workout.update(workout_params)
+            redirect_to @workout
+        else
+            render 'edit'
+        end
     end
 
     def edit
     end
 
     def destroy
+        @workout.destroy
+        redirect_to root_path
     end
  
 
